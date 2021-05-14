20fbcec3fe2b7487547a4c6cf4ef2899fb912e82
Jonathan Lindén <jonathan@jlinden.se>
2021-05-14 17:22:27 +0200

    Updated tests to calculate cycle losses and output the best sample.

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import sys
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 import torch
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
@@ -26,6 +29,8 @@ if cuda:
     netG_A2B.cuda()
     netG_B2A.cuda()
 
+criterion_cycle = torch.nn.L1Loss()
+
 # Load state dicts
 netG_A2B.load_state_dict(torch.load(generator_A2B))
 netG_B2A.load_state_dict(torch.load(generator_B2A))
@@ -43,20 +48,76 @@ dataloader = DataLoader(
     ImageDataset(dataroot, transforms_=transforms_, mode='test'),
     batch_size=batch_size, shuffle=False, num_workers=n_cpu)
 
+best_cycle_loss = np.inf
+best_cycle_image = {}
+loss_cycle_ABA_list = []
+loss_cycle_BAB_list = []
+
 for i, batch in enumerate(dataloader):
     # Set model input
     real_A = Variable(input_A.copy_(batch['A']))
     real_B = Variable(input_B.copy_(batch['B']))
 
     # Generate output
-    fake_B = 0.5 * (netG_A2B(real_A).data + 1.0)
-    fake_A = 0.5 * (netG_B2A(real_B).data + 1.0)
+    fake_B_raw = netG_A2B(real_A).data
+    fake_A_raw = netG_B2A(real_B).data
+    fake_B = 0.5 * (fake_B_raw + 1.0)
+    fake_A = 0.5 * (fake_A_raw + 1.0)
 
     # Save image files
     save_image(fake_A, 'output/A/%04d.png' % (i + 1))
     save_image(fake_B, 'output/B/%04d.png' % (i + 1))
 
+    recovered_A = 0.5 * (netG_B2A(fake_B_raw) + 1.0)
+    recovered_B = 0.5 * (netG_A2B(fake_A_raw) + 1.0)
+
+    save_image(recovered_A, f'output/recovered/A/{(i+1):04d}.png')
+    save_image(recovered_B, f'output/recovered/B/{(i+1):04d}.png')
+
+    loss_cycle_ABA = criterion_cycle(recovered_A, real_A)
+    loss_cycle_BAB = criterion_cycle(recovered_B, real_B)
+
+    loss_cycle_ABA_list.append(loss_cycle_ABA.item())
+    loss_cycle_BAB_list.append(loss_cycle_BAB.item())
+
+    if loss_cycle_ABA < best_cycle_loss and loss_cycle_ABA < loss_cycle_BAB:
+        best_cycle_loss = loss_cycle_ABA
+        best_cycle_image = {
+            'real': 0.5 * (real_A.cpu() + 1.0).squeeze(0).permute(1, 2, 0).numpy(),
+            'fake': fake_B.cpu().squeeze(0).permute(1, 2, 0).numpy(),
+            'recovered': recovered_A.cpu().squeeze(0).permute(1, 2, 0).detach().numpy()
+        }
+    elif loss_cycle_BAB < best_cycle_loss:
+        best_cycle_loss = loss_cycle_BAB
+        best_cycle_image = {
+            'real': 0.5 * (real_B.cpu() + 1.0).squeeze(0).permute(1, 2, 0).numpy(),
+            'fake': fake_A.cpu().squeeze(0).permute(1, 2, 0).numpy(),
+            'recovered': recovered_B.cpu().squeeze(0).permute(1, 2, 0).detach().numpy()
+        }
     sys.stdout.write(
         '\rGenerated images %04d of %04d' % (i + 1, len(dataloader)))
 
 sys.stdout.write('\n')
+
+real = best_cycle_image['real']
+fake = best_cycle_image['fake']
+recovered = best_cycle_image['recovered']
+
+plt.tight_layout()
+fig, ax = plt.subplots(1, 3, sharey='all')
+ax[0].imshow(real)
+ax[0].axis('off')
+ax[0].set_title('Real')
+ax[1].imshow(fake)
+ax[1].axis('off')
+ax[1].set_title('Fake')
+ax[2].imshow(recovered)
+ax[2].axis('off')
+ax[2].set_title('Recovered')
+plt.savefig('figures/best_cycle.png', bbox_inches='tight', pad_inches=0,
+            dpi=300)
+plt.show()
+
+print(f'Best cycle loss: {best_cycle_loss}')
+print(f'Average ABA loss: {np.mean(loss_cycle_ABA_list)}')
+print(f'Average BAB loss: {np.mean(loss_cycle_BAB_list)}')
